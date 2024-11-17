4f71d62e8fdfdeb80c4752ae178acd4b8b643edc
DhruvalG <dhruval.gv@gmail.com>
2024-11-18 01:50:44 +0530

    backend fix

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb://qkart-node-shard-00-00.uoufb.mongodb.net:27017,qkart-node-shard-00-01.uoufb.mongodb.net:27017,qkart-node-shard-00-02.uoufb.mongodb.net:27017/qkart?replicaSet=atlas-i8y0ej-shard-0" --ssl --authenticationDatabase admin --username dhruvalgv --password 0QjN36mGYvmNkCSn --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb://qkart-node-shard-00-00.uoufb.mongodb.net:27017,qkart-node-shard-00-01.uoufb.mongodb.net:27017,qkart-node-shard-00-02.uoufb.mongodb.net:27017/qkart?replicaSet=atlas-i8y0ej-shard-0" --ssl --authenticationDatabase admin --username dhruvalgv --password 0QjN36mGYvmNkCSn --drop --collection products --file data/export_qkart_products.json
